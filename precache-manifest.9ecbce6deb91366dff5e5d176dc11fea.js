e6ff134e5867fefb3df529d4dda3d65ec5dc74b2
WildCodeSchool <anelisynagano@gmail.com>
2020-09-14 14:00:25 +0100

    Updates

---
+++ precache-manifest.9ecbce6deb91366dff5e5d176dc11fea.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e698885aad02bc7501a704dd12d66296",
+    "url": "/nokia-hackathon-group-2/index.html"
+  },
+  {
+    "revision": "300f0cce25cf78cb7468",
+    "url": "/nokia-hackathon-group-2/static/css/2.e6cf9162.chunk.css"
+  },
+  {
+    "revision": "dc7372dec0664ba4d690",
+    "url": "/nokia-hackathon-group-2/static/css/main.d1544536.chunk.css"
+  },
+  {
+    "revision": "300f0cce25cf78cb7468",
+    "url": "/nokia-hackathon-group-2/static/js/2.8ffa5f5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nokia-hackathon-group-2/static/js/2.8ffa5f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7372dec0664ba4d690",
+    "url": "/nokia-hackathon-group-2/static/js/main.794f7a59.chunk.js"
+  },
+  {
+    "revision": "3c2ca555b1dd034d3eff",
+    "url": "/nokia-hackathon-group-2/static/js/runtime-main.5091b1fe.js"
+  },
+  {
+    "revision": "e1f1cdc9c8afc51e38aef9114893209f",
+    "url": "/nokia-hackathon-group-2/static/media/NokiaPureHeadline_Bd.e1f1cdc9.ttf"
+  },
+  {
+    "revision": "2cb2263e964fdc30c500094435cc3b30",
+    "url": "/nokia-hackathon-group-2/static/media/NokiaPureHeadline_Lt.2cb2263e.ttf"
+  },
+  {
+    "revision": "d10dc635a994f5d8ce20b91d4273705d",
+    "url": "/nokia-hackathon-group-2/static/media/NokiaPureHeadline_Rg.d10dc635.ttf"
+  },
+  {
+    "revision": "d3eadc8d2baa41ab6ffebac26a3cf6b3",
+    "url": "/nokia-hackathon-group-2/static/media/NokiaPureTWeb_Bd.d3eadc8d.ttf"
+  },
+  {
+    "revision": "abb1b26db4e48c3b62940e45b06d87f9",
+    "url": "/nokia-hackathon-group-2/static/media/NokiaPureTWeb_Lt.abb1b26d.ttf"
+  },
+  {
+    "revision": "536160e84aba3fb83504066dd068bed3",
+    "url": "/nokia-hackathon-group-2/static/media/NokiaPureTWeb_Rg.536160e8.ttf"
+  }
+]);
